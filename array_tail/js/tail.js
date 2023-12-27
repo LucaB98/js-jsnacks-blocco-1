cdac7c06182cc54fda6234f76f760cc75236f704
LucaB98 <lucabellan98@gmail.com>
2023-12-27 12:00:23 +0100

    scaffolding

---
+++ array_tail/js/tail.js
@@ -0,0 +1,7 @@
+console.log('JS OK')
+
+const input = document.getElementById('input');
+const button = document.getElementById('button');
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+const error = document.querySelector('.invalid-feedback');
